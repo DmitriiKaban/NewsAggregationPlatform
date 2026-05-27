cbce5590fdada47f7e7805358e683b2a0a81299b
mirrerror <cayote20@mail.ru>
2026-05-28 00:05:34 +0300

    update kubernetes setup

--- k8s/stop-k8s.sh
+++ k8s/stop-k8s.sh
@@ -1,11 +1,8 @@
-Write-Host "Hibernating services to save RAM..." -ForegroundColor Magenta
+#!/bin/bash
 
-# 1. Scale everything to zero
+echo "Hibernating services to save RAM..."
+
 kubectl scale deployment --all --replicas=0
 kubectl scale statefulset --all --replicas=0
 
-# 2. Kill the ngrok process
-Write-Host "Closing Ngrok..." -ForegroundColor Red
-Stop-Process -Name "ngrok" -ErrorAction SilentlyContinue
-
-Write-Host "All pods terminated. Let the PC breathe." -ForegroundColor White
+echo "All pods terminated. Let the PC breathe."
